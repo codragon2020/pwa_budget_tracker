dd8574d5f44b5303068308e34de4706813359acc
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-02 06:31:37 -0400

    Added service worker script to register in index.html. Added few comments to serviceWorker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,5 @@
+// The Service Worker API enables the creation of effective offline experiences, intercepts network requests and take appropriate action based on whether the network is available, and update assets residing on the server
+
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
@@ -11,7 +13,11 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-self.addEventListener("install", function (evt) {
+// Service Worker lifecycle has 3 phases: Download, Install, and Activate
+
+// Installation is attempted when the downloaded file is found to be new - either different to an existing service worker, or the first service worker encountered for the page/site
+self.addEventListener("install", function(evt) {
+    // Because install/activate events could take a while to complete, the service worker spec provides a waitUntil() method
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             console.log("Your files have been pre-cached successfully!");
@@ -21,6 +27,7 @@ self.addEventListener("install", function (evt) {
     self.skipWaiting();
 })
 
+// The new service worker is only activated when there are no longer any pages loaded that are still using the old service worker. As soon as there are no more pages to be loaded, the new service worker activates (becoming the active worker)
 self.addEventListener("activate", function (evt) {
     evt.waitUntil(
         caches.keys().then(keyList => {
@@ -38,25 +45,29 @@ self.addEventListener("activate", function (evt) {
 });
 
 self.addEventListener("fetch", function (evt) {
+    // cache successful requests to the API
     if(evt.request.url.includes("/api/transaction")) {
         console.log("[Service Worker] Fetch (data)", evt.request.url);
-
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(evt.request)
                     .then(response => {
+                        // If response was good, clone it and store it in cache
                         if (response.status === 200) {
                             cache.put(evt.request.url, response.clone());
                         }
                         return response;
                     })
                     .catch(err => {
+                        // Network request failed, try to get it from the cache
                         return cache.match(evt.request);
                     });
             })
         );
         return;
     }
+
+    // if the request is not for the API, serve static assets using "offline-first" approach.
     evt.respondWith(
         caches.open(CACHE_NAME).then(cache => {
             return cache.match(evt.request).then(response => {
